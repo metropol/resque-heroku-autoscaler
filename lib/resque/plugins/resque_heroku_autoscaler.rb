5f82357b8a25e41bfef8e14e1becc7640ed9f08e
Ugo Di Profio <gmetropol@gmail.com>
2011-08-01 23:29:59 +0900

    Removed check on Resque's workers count.
    Resque.info[:workers] seems unreliable because even if the worker is
    killed (with ps_scale to 0), the 'worker' key in Resque.redis
    persists, and the worker is a dead worker.
    Dead workers will be pruned by Resque upon starting a new worker (see
    prune_dead_workers).

--- lib/resque/plugins/resque_heroku_autoscaler.rb
+++ lib/resque/plugins/resque_heroku_autoscaler.rb
@@ -8,7 +8,6 @@ module Resque
 
       def after_enqueue_scale_workers_up(*args)
         if !Resque::Plugins::HerokuAutoscaler::Config.scaling_disabled? && \
-          Resque.info[:workers] == 0 && \
           Resque::Plugins::HerokuAutoscaler::Config.new_worker_count(Resque.info[:pending]) >= 1
           set_workers(1)
           Resque.redis.set('last_scaled', Time.now)
